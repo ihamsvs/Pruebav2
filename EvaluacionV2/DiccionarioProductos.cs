0722daecc1c4922acf0066c96c05df72507d5770
Iham Vivanco <ihamvivanco5@gmail.com>
2022-11-17 13:10:43 -0300

    Creacion del menu principal

---
+++ EvaluacionV2/DiccionarioProductos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluacionV2
+{
+    class DiccionarioProductos
+    {
+        private Dictionary<int, string> diccionarioP = new Dictionary<int, string>();
+
+        public void AgregarProducto(int key, string nombre)
+        {
+            diccionarioP.Add(key, nombre);
+        }
+
+
+        public void MostrarProductos(int key)
+        {
+            string nombre = "";
+            Console.WriteLine("\n --------------------- \n");
+            Console.WriteLine("Buscando un producto con la key " + key);
+
+            if(diccionarioP.TryGetValue(key, out nombre))
+            {
+                Console.WriteLine("Nombre "+ nombre)
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No existe esa key en la base de datos");
+            }
+            
+
+
+
+        }
+
+    }
+}
--- EvaluacionV2/Program.cs
+++ EvaluacionV2/Program.cs
@@ -10,7 +10,51 @@ namespace EvaluacionV2
     {
         static void Main(string[] args)
         {
+            #region declaracion de variables y obejtos
+            bool exit = false;
+            DiccionarioProductos p = new DiccionarioProductos();
+            #endregion
 
+            while (!exit)
+            {
+                Console.WriteLine("-------------- Bienvenido a Trueque --------------");
+                Console.WriteLine("1 - ¿ Desea intercambiar un articulo ? ");
+                Console.WriteLine("2 - ¿ Buscar un producto en la bodega ?");
+                Console.WriteLine("3 - ¿ Agregar un producto a la bodega ?");
+                Console.WriteLine("4 - Mostrar productos en bodega");
+
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Intercambiar producto");
+                        break;
+                    case 2:
+                        Console.WriteLine("Nombre del producto");
+                        break;
+                    case 3:
+                        Console.WriteLine("Que producto desea agregar");
+                        Console.WriteLine("Ingrese la llave del producto"); 
+                        int key = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el nombre del producto");
+                        string nombre = Console.ReadLine();
+                        p.AgregarProducto(key, nombre);
+                        break;
+                    case 4:
+                        Console.WriteLine("Ingrese la key del producto");
+                        int key = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\n ----- Mostrando productos en bodega ----- \n ");
+                        p.MostrarProductos(key);
+
+
+
+                }
+            
+            
+            
+            
+                Console.ReadKey();
+            }   
         }
     }
-}
